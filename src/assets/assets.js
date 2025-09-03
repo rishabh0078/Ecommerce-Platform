fb490f1e94ee683d33cb147204117604cb3cfdfa
rishabh <rishabhchaudhary058@gmail.com>
2025-09-03 10:52:41 +0530

    commit

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -57,7 +57,7 @@ import p_img52 from './p_img52.png'
 
 import logo from './logo.png'
 import hero_img from './hero_img.png'
-import hero_img1 from './hero_img1.png'
+import hero_img1 from './hero_img1.jpg'
 import cart_icon from './cart_icon.png'
 import bin_icon from './bin_icon.png'
 import dropdown_icon from './dropdown_icon.png'
